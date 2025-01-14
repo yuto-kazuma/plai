35753bc6741745a03312e2f8f252dc0482db4bfb
yuto-kazuma <takumi.yuto@gmx.com>
2025-01-15 03:24:25 +0800

    feat: add new logo images and update LogoSymbol component for dark mode support

--- apps/web/src/components/web/ui/logo.tsx
+++ apps/web/src/components/web/ui/logo.tsx
@@ -7,10 +7,9 @@ import { cx } from "~/utils/cva"
 
 export const Logo = ({ className, ...props }: ComponentProps<typeof Stack>) => {
   return (
-    <Stack size="sm" className={cx("group/logo text-foreground", className)} asChild {...props}>
-      <Link href="/" prefetch={false}>
+    <Stack size="sm" className={cx("group/logo text-foreground gap-0  ", className)} asChild {...props}>
+      <Link className="gap-2" href="/" prefetch={false}>
         <LogoSymbol className="duration-300! ease-in-out! will-change-transform group-hover/logo:rotate-90" />
-
         <span className="font-medium text-sm">{config.site.name}</span>
       </Link>
     </Stack>
